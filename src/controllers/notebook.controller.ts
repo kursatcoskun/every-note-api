fe6bee863dae44649348d574e4c8be09fbe63d75
Kürşat Coşkun <kursat.coskun@etiya.com>
2020-12-27 18:08:55 +0300

    add update notebook controller method

--- src/controllers/notebook.controller.ts
+++ src/controllers/notebook.controller.ts
@@ -1,3 +1,4 @@
+import { Resolver } from 'dns';
 import { NextFunction, Request, Response } from 'express';
 import { NotebookDto } from '../dtos/notebook.dto';
 import { CreateUserDto } from '../dtos/users.dto';
@@ -5,6 +6,7 @@ import { NotebookEntity } from '../entity';
 import { User } from '../interfaces/users.interface';
 import { NotebookService } from '../services';
 import { GenericResponse } from '../utils/genericResponse';
+import { GenericResult } from '../utils/genericResult';
 
 export class NotebookController {
   public notebookService = new NotebookService();
@@ -25,6 +27,17 @@ export class NotebookController {
       const notebookDto: NotebookDto = req.body;
       const createdNotebook: NotebookDto = await this.notebookService.createNotebook(notebookDto);
       const genericResponse: GenericResponse<NotebookDto> = { data: createdNotebook, result: { resultType: 'SUCCESS' } };
+      res.status(200).json(genericResponse);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateNotebook = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const notebookDto: NotebookDto = req.body;
+      const updatedNotebook: NotebookDto = await this.notebookService.updateNotebook(notebookDto);
+      const genericResponse: GenericResponse<NotebookDto> = { data: updatedNotebook, result: { resultType: 'SUCCESS' } };
       res.status(200).json(genericResponse);
     } catch (error) {
       next(error);
