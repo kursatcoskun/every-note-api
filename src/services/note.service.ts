df0ee03f20c4b907a24b563841adaa6917b239ab
Kürşat Coşkun <kursat.coskun@etiya.com>
2021-01-02 20:53:16 +0300

    note.service added all crud methods

--- src/services/note.service.ts
+++ src/services/note.service.ts
@@ -23,4 +23,22 @@ export class NoteService {
     if (!savedEntity) throw new HttpException(500, 'Note can not created.');
     return plainToClass(NoteDto, savedEntity);
   }
+
+  public async updateNote(noteDto: NoteDto): Promise<NoteDto> {
+    const noteEntity = plainToClass(NoteEntity, noteDto);
+    const noteRepository = getRepository(this.noteEntity);
+    const updatedNote = await noteRepository.update(noteEntity.id, noteEntity);
+    if (!updatedNote) throw new HttpException(500, 'Note can not updated.');
+    return plainToClass(NoteDto, updatedNote);
+  }
+
+  public async deleteNote(noteId: number): Promise<Boolean> {
+    const noteRepository = getRepository(this.noteEntity);
+    const noteEntity = await noteRepository.findOne({ where: { id: noteId } });
+    if (!noteEntity) throw new HttpException(404, 'Note can not be found. Note ID: ' + noteId);
+
+    const deleted = await noteRepository.delete({ id: noteId });
+
+    return !!deleted;
+  }
 }
